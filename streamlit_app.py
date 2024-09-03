1726af6c2ac1730748a1c528d8fc6b787dbfe960
Prakash-chem <158026336+Prakash-chem@users.noreply.github.com>
2024-09-03 11:48:02 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,18 +8,15 @@ import sys
 from PIL import Image
 
 # Clone the repository (if needed)
-# This step can be commented out if the repository is already included in your deployment
 if not os.path.exists('optimalpH_colab'):
     subprocess.run(['git', 'clone', 'https://github.com/Loganz97/optimalpH_colab.git'])
 
-# Ensure the correct Python path
 sys.path.append('optimalpH_colab')
 
 # Display the image
 image = Image.open('pH.png')
 st.image(image, use_column_width=True)
 
-# Model Selection
 model_selection = st.selectbox("Choose a model", ["XGBoost (Most Accurate)", "KNN (Also Performs Well)", "K-mers"])
 
 model_map = {
@@ -43,12 +40,7 @@ def calculate_protein_properties(sequence):
         isoelectric_point = analysis.isoelectric_point()
         instability_index = analysis.instability_index()
         
-        if instability_index <= 30:
-            instability_gauge = "Stable"
-        elif 30 < instability_index <= 40:
-            instability_gauge = "Moderately Stable"
-        else:
-            instability_gauge = "Unstable"
+        instability_gauge = "Stable" if instability_index <= 30 else "Moderately Stable" if instability_index <= 40 else "Unstable"
         
         lysine_count = sequence.count('K')
         arginine_count = sequence.count('R')
@@ -62,7 +54,6 @@ def calculate_protein_properties(sequence):
         st.error(f"Error processing sequence: {e}")
         return tuple([None] * 9)
 
-# Upload CSV file
 st.write("Upload your CSV file containing protein sequences:")
 uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
@@ -89,7 +80,6 @@ if uploaded_file:
         df[['ID', 'Sequence']].to_csv(input_file, index=False)
         output_file = 'optimalpH_results.csv'
 
-        # Download model weights
         model_id = "Loganz97/optimalpH"
         model_files = ["model_kmers", "model_knn", "model_xgboost"]
         local_weights_paths = {}
